3db127375478d281324c83249ec119a6cf3cd0f8
sahwaelina-cmyk <sahwaelina@gmail.com>
2026-09-12 16:43:22 +0700

    feat: tambah section jam buka dan lokasi UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-    promoButton.textContent = "Promo: Beli 2 gratis tester!";
-    console.log("Promo Kopi Nusa berhasil ditampilkan.");
+    promoButton.textContent = "Promo: Gratis 1 kotak untuk pemesanan 20 kotak!";
+    console.log("Promo Dapur Mak Win berhasil ditampilkan.");
 });
